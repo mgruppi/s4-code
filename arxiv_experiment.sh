726a7812b3bd493f19b2c18e0659d40a6fd7588a
gruppi <mauricio.gruppi@gmail.com>
2020-08-18 19:13:15 -0400

    Add US-UK experiment to this setup

--- arxiv_experiment.sh
+++ arxiv_experiment.sh
@@ -3,6 +3,9 @@
 cat1="wordvectors/arxiv/arXiv_category_cs.AI.vec"
 cat2="wordvectors/arxiv/arXiv_category_physics.class-ph.vec"
 
+# cat1="wordvectors/ukus/eng_uk_bnc.vec"
+# cat2="wordvectors/ukus/eng_us_coca.vec"
+
 # Compute semantic shifts
 python3 arxiv.py $cat1 $cat2
 
