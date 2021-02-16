e721bb76dfaadf8dd4e12f0cd6ac2f10c8fcd60f
mgruppi <mauricio.gruppi@gmail.com>
2021-02-16 18:18:22 -0500

    Update

--- cord_experiment.sh
+++ cord_experiment.sh
@@ -10,7 +10,8 @@ cat2="../data/news-group3.vec"
 # cat2="wordvectors/ukus/eng_us_coca.vec"
 
 # Compute semantic shifts
-python3 cord19.py $cat1 $cat2
+# python3 cord19.py $cat1 $cat2
 
 # Produce table of results
-python3 arxiv_table.py "results/arxiv/cord-19.vec-news-group3.vec.csv" > "results/arxiv/table.md"
+# python3 arxiv_table.py "results/arxiv/cord-19.vec-news-group3.vec.csv" > "results/arxiv/table.md"
+python3 arxiv_table.py "results/arxiv/cord-19.vec-coca-newspaper-all.vec.csv" > "results/arxiv/table.md"
