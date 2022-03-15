30075d960b040e8bb71e7fc0737a70c1b2db8960
mgruppi <mauricio.gruppi@gmail.com>
2022-03-15 10:31:57 -0400

    Add arguments

--- plot_param_search_results.py
+++ plot_param_search_results.py
@@ -1,32 +1,51 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-df = pd.read_csv("results_param_search_semeval.txt", sep="|", header=0, index_col=False,
-                 names=["language", "mean_accuracy", "max_accuracy", "r"])
+files = ["param_search_results_semeval_cosine.txt", "param_search_results_semeval_s4.txt"]
 
-print(df)
-
-for lang in df["language"].unique():
-    print(lang)
-    x = df[df["language"] == lang]
-    plt.plot(x["r"], x["mean_accuracy"], label=lang)
-
-
-plt.xlabel("r")
-plt.ylabel("acc")
-plt.legend()
-plt.show()
+for f in files:
+    df = pd.read_csv(f, index_col=False)
+    sns.relplot(data=df, x="r", y="accuracy", kind="line", hue="language")
+    plt.legend()
+    plt.show()
 
 
-df_u = pd.read_csv("results_r_ukus.txt", sep="|", header=0, index_col=False,
-                   names=["cls_method", "align_method", "acc", "prec", "recall", "f1", "r"])
+files_ukus = ["param_search_results_ukus_cosine_03.txt", "param_search_results_ukus_s4.txt"]
 
-for cls in df_u["cls_method"].unique():
-    print(cls)
-    x = df_u[df_u["cls_method"] == cls]
-    plt.plot(x["r"], x["acc"], label=cls)
+for f in files:
+    df = pd.read_csv(f, index_col=False)
+    sns.relplot(data=df, x="r", y="accuracy", kind="line")
+    plt.legend()
+    plt.show()
 
-plt.xlabel("r")
-plt.ylabel("acc")
-plt.legend()
-plt.show()
+
+# df = pd.read_csv("results_param_search_semeval.txt", sep="|", header=0, index_col=False,
+#                  names=["language", "mean_accuracy", "max_accuracy", "r"])
+#
+# print(df)
+#
+# for lang in df["language"].unique():
+#     print(lang)
+#     x = df[df["language"] == lang]
+#     plt.plot(x["r"], x["mean_accuracy"], label=lang)
+#
+#
+# plt.xlabel("r")
+# plt.ylabel("acc")
+# plt.legend()
+# plt.show()
+#
+#
+# df_u = pd.read_csv("results_r_ukus.txt", sep="|", header=0, index_col=False,
+#                    names=["cls_method", "align_method", "acc", "prec", "recall", "f1", "r"])
+#
+# for cls in df_u["cls_method"].unique():
+#     print(cls)
+#     x = df_u[df_u["cls_method"] == cls]
+#     plt.plot(x["r"], x["acc"], label=cls)
+#
+# plt.xlabel("r")
+# plt.ylabel("acc")
+# plt.legend()
+# plt.show()
