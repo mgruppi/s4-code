0ad42345c50efda7a0f3dfad950f5c7b03a92f46
mgruppi <mauricio.gruppi@gmail.com>
2022-03-21 23:00:47 -0400

    Update plots

--- plot_param_search_results.py
+++ plot_param_search_results.py
@@ -2,22 +2,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-files = ["param_search_results_semeval_cosine.txt", "param_search_results_semeval_s4.txt"]
+f_se = "param_search_results_semeval.txt"
+f_en = "param_search_results_ukus.txt"
 
-for f in files:
-    df = pd.read_csv(f, index_col=False)
-    sns.relplot(data=df, x="r", y="accuracy", kind="line", hue="language")
+df_se = pd.read_csv(f_se)
+
+print(df_se)
+lang = "english"
+cls = "cosine_010"
+metric = "accuracy"
+
+languages = ["english"]
+for lang in languages:
+    df = df_se[df_se["language"] == lang]
+    sns.relplot(data=df, x="r", y="accuracy", kind="line", hue="cls_name")
     plt.legend()
     plt.show()
 
 
-files_ukus = ["param_search_results_ukus_cosine_03.txt", "param_search_results_ukus_s4.txt"]
-
-for f in files:
-    df = pd.read_csv(f, index_col=False)
-    sns.relplot(data=df, x="r", y="accuracy", kind="line")
-    plt.legend()
-    plt.show()
+# files = ["param_search_results_semeval_cosine.txt", "param_search_results_semeval_s4.txt"]
+#
+# for f in files:
+#     df = pd.read_csv(f, index_col=False)
+#     sns.relplot(data=df, x="r", y="accuracy", kind="line", hue="language")
+#     plt.legend()
+#     plt.show()
+#
+#
+# files_ukus = ["param_search_results_ukus_cosine_03.txt", "param_search_results_ukus_s4.txt"]
+#
+# for f in files:
+#     df = pd.read_csv(f, index_col=False)
+#     sns.relplot(data=df, x="r", y="accuracy", kind="line")
+#     plt.legend()
+#     plt.show()
 
 
 # df = pd.read_csv("results_param_search_semeval.txt", sep="|", header=0, index_col=False,
