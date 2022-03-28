f8b1e082ec20e5314ac2b4e6a81f3b3ddb166965
mgruppi <mauricio.gruppi@gmail.com>
2022-03-28 17:50:46 -0400

    S4 overhaul. Use PyTorch for neural network. Optimized many operations that were poorly written. Added time report if `--verbose` is on.

--- s4_torch.py
+++ s4_torch.py
@@ -88,10 +88,10 @@ if __name__ == "__main__":
     model = S4Network(wv1.dimension*2)
 
     l, m, q = s4(wv1, wv2,
-                 cls_model="nn",
+                 cls_model=model,
                  n_targets=100,
                  n_negatives=100,
                  rate=1,
-                 iters=100,
+                 iters=500,
                  verbose=1,
                  update_landmarks=True)
