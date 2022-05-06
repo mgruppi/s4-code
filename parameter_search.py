0884384a49e46cf1f0afe6c85bde2b7b60c09b2d
mgruppi <mauricio.gruppi@gmail.com>
2022-05-06 17:42:30 -0400

    Generalize experiments into a single method

--- parameter_search.py
+++ parameter_search.py
@@ -5,13 +5,14 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from WordVectors import WordVectors, intersection
 import numpy as np
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from s4 import s4, threshold_crossvalidation
 from s4_torch import S4Network
 from scipy.spatial.distance import cosine
 from param_search_semeval import get_feature_cdf, vote
 from alignment import align
 import argparse
+import itertools
 
 
 def cosine_cls(wv1, wv2, targets_1, targets_2, y_true, threshold=0.5, **kwargs):
@@ -23,8 +24,9 @@ def cosine_cls(wv1, wv2, targets_1, targets_2, y_true, threshold=0.5, **kwargs):
     precision = precision_score(y_true, y_bin)
     recall = recall_score(y_true, y_bin)
     f1 = f1_score(y_true, y_bin)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_bin).ravel()
 
-    return accuracy, precision, recall, f1
+    return accuracy, precision, recall, f1, tn, fp, fn, tp
 
 
 def s4_cls(wv1, wv2, targets_1, targets_2, y_true, **kwargs):
@@ -41,9 +43,10 @@ def s4_cls(wv1, wv2, targets_1, targets_2, y_true, **kwargs):
     precision = precision_score(y_true, y_bin)
     recall = recall_score(y_true, y_bin)
     f1 = f1_score(y_true, y_bin)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
 
-    return accuracy, precision, recall, f1
-
+    return accuracy, precision, recall, f1, tn, fp, fn, tp
+     
 
 def choice_experiments(wv1, wv2, targets_1, targets_2, y_true, num_trials=10, r=1.0,
                     cls=cosine_cls,
@@ -260,7 +263,7 @@ def read_ukus_data(normalized=False):
     return wv_uk, wv_us, dico, y_true
 
 
-if __name__ == "__main__":
+def old_main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("param", type=str, choices=["r", "n", "choice_method"],
@@ -425,3 +428,195 @@ if __name__ == "__main__":
                     if len(res) > 0:
                         print(name, *res, sep=",", file=fout)
 
+
+def run_experiments(wv1, wv2, targets_1, targets_2, y_true,
+                    r, n_pos, n_neg, choice_method, align_method, classifier,
+                    num_trials=10,
+                    **kwargs):
+    """
+    Performs experiments with a given setup. The number of settings is determined by which parameters are variable.
+    Particularly, each of the `r`, `n_pos`, `n_neg`, `choice_method`, `align_method`, and `cls` can be varied, resulting in up to
+    `s * t * u * v * w * x`, parameter combinations. 
+    Typically, several parameters would be fixed and only one or few varied.
+    Args:
+        wv1:    (WordVectors)
+        wv2:    (WordVectors)
+        targets_1:  (iterable) Target words in wv1  
+        targets_2:  (iterable) Target words in wv2
+        y_true: (iterable) True label of the target words
+        r:      (float or iterable) If `float`, the fixed value of parameter `r`. If iterable, the list of values of `r` to apply.
+        n_pos:  (float or iterable) If `float`, the fixed number of positive samples to generate. If `iterable`, the list of possible `n_pos` values.
+        n_neg:  (float or iterable) If `float`, the fixed number of negative samples to generate. If `iterable`, the list of possible `n_neg` values.
+        choice_method:  (string or iterable) If `string`, the fixed perturbation method to apply. If `iterable`, the list of perturbations method to use.
+        align_method:   (string or iterable) If `string`, the fixed alignment method to use. If `iterable`, the list of alignment methods to use
+        classifier:  (tuple or iterable) If `tuple`, then `(name, func, threshold)` must be passed as the name, callable and threshold of the classifier. If `iterable`, the list of classifier names)
+        num_trials: (int) The number of trials to run for each setting.
+        **kwargs:   Keyword arguments to send to S4.
+    Returns:
+        (header, results): Generator for the experiments. `header` is a tuple containing the name of the returned fields,
+        `results` is a tuple containing the value of the returned fields.
+    """
+    np.random.seed(1)
+    if not hasattr(r, '__iter__'):
+        r = [r]  # Turn fixed r in to a list
+    if not hasattr(n_pos, '__iter__'):
+        n_pos = [n_pos]
+    if not hasattr(n_neg, '__iter__'):
+        n_neg = [n_neg]
+    if type(choice_method) == str or not hasattr(choice_method, '__iter__'):
+        choice_method = [choice_method]
+    if type(align_method) == str or not hasattr(align_method, '__iter__'):
+        align_method = [align_method]
+    if not hasattr(classifier, '__iter__'):
+        classifier = [classifier]
+    
+    exp_settings = itertools.product(r, n_pos, n_neg, choice_method, align_method, classifier)
+    for _r, _np, _nn, _cm, _am, _cls in exp_settings:
+        print(_r, _np, _nn, _cm, _am, _cls)
+
+        for i in range(num_trials):
+            if _am == "global":
+                landmarks = wv1.words
+            elif _am == "s4a":
+                cls_model = S4Network(wv1.dimension*2)  # This should not be the classifier from `_cls`, this is the internal S4 model
+                landmarks, non_landmarks, Q, =  s4(wv1, wv2,
+                                                    verbose=0,
+                                                    rate=_r,
+                                                    n_targets=_np,
+                                                    n_negatives=_nn,
+                                                    cls_model=cls_model,
+                                                    iters=2,  # TODO: change back to 10-20 range
+                                                )
+            n_landmarks = len(landmarks)
+            header_tuple = ('r', 'n_pos', 'n_neg', 'choice_method', 'alignment', 'cls', 'accuracy', 'precision',
+                'recall', 'f1', 'true_negatives', 'false_positives', 'false_negatives', 'true_positives')
+            if n_landmarks > 0:
+                wv1_, wv2_, Q = align(wv1, wv2, anchor_words=landmarks)
+                acc, prec, rec, f1, tn, fp, fn, tp = _cls[1](wv1_, wv2_, targets_1, targets_2, y_true, landmarks=landmarks, threshold=_cls[2])
+                res_tuple = (_r, _np, _nn, _cm, _am, _cls[0], acc, prec, rec, f1, tn, fp, fn, tp)
+            else:
+                res_tuple = (_r, _np, _nn, _cm, _am, _cls[0], -1, -1, -1, -1, -1, -1, -1, -1)
+            yield (header_tuple, res_tuple)
+
+
+def new_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("param", type=str, choices=["r", "n", "choice_method", "all"],
+                        help="Type of parameter to search. r - rate of perturbation | n - no. of samples to perturb")
+    parser.add_argument("--no-semeval", dest="no_semeval", action="store_true",
+                        help="Do not perform SemEval 2020 experiment")
+    parser.add_argument("--no-ukus", dest="no_ukus", action="store_true",
+                        help="Do not perform UKUS experiment")
+    parser.add_argument("--num-trials", dest="num_trials", type=int, default=10,
+                        help="Number of trials per r value")
+    parser.add_argument("--normalized", action="store_true", help="Normalize word vectors")
+    parser.add_argument("--languages", default=None, nargs="+", help="List of languages")
+    parser.add_argument("--r-upper", dest="r_upper", default=2, type=float, help="Upper bound for r")
+    parser.add_argument("--r-steps", dest="r_steps", default=11, type=int, help="No. of steps for r")
+    parser.add_argument("--output-file", dest='output_file', default='results_param_search.csv', type=str,
+                        help='Change default output file')
+
+    args = parser.parse_args()
+
+    normalized = args.normalized
+
+    if args.languages is None:
+        languages = ["english", "german", "latin", "swedish"]
+    else:
+        languages = args.languages
+
+    # Make file header
+    header_tuple = ('dataset', 'normalized', 'r', 'n_pos', 'n_neg', 'choice_method', 'alignment', 'cls', 'accuracy', 'precision',
+    'recall', 'f1', 'true_negatives', 'false_positives', 'false_negatives', 'true_positives')
+    fout = open(args.output_file, 'w')
+    print(*header_tuple, sep=',', file=fout)
+
+    if args.param == "all":  # all vs all comparison
+        n_pos_range = np.arange(500, 5500, 500)
+        print("N_pos range", n_pos_range)
+        n_neg_range = np.arange(500, 5500, 500)
+        print("N_neg range", n_neg_range)
+
+        r_range = np.linspace(0, 5, 10)
+        print("R-range", r_range)
+
+        choice_methods = ['random', 'far', 'close']
+        print("Choice methods", choice_methods)
+
+        align_methods = ['s4a']
+
+        cls_names = ['cosine_025', 'cosine_050', 'cosine_075', 'cosine_090']
+        cls_func = [cosine_cls, cosine_cls, cosine_cls, cosine_cls]
+        cls_thresholds = [0.25, 0.5, 0.75, 0.90]
+        classifiers = list(zip(cls_names, cls_func, cls_thresholds))
+    elif args.param == "r":
+        n_pos_range = [500]
+        n_neg_range = [500]
+        choice_methods = ['random']
+        align_methods = ['s4a']
+        cls_names = ['cosine_025', 'cosine_050', 'cosine_075', 'cosine_090']
+        cls_func = [cosine_cls, cosine_cls, cosine_cls, cosine_cls]
+        cls_thresholds = [0.25, 0.5, 0.75, 0.90]
+        classifiers = list(zip(cls_names, cls_func, cls_thresholds))
+        r_range = np.linspace(0, 5, 10) 
+        print("R-range", r_range)
+    elif args.param == "n":
+        choice_methods = ['random']
+        align_methods = ['s4a']
+        cls_names = ['cosine_025', 'cosine_050', 'cosine_075', 'cosine_090']
+        cls_func = [cosine_cls, cosine_cls, cosine_cls, cosine_cls]
+        cls_thresholds = [0.25, 0.5, 0.75, 0.90]
+        classifiers = list(zip(cls_names, cls_func, cls_thresholds))
+        r_range [2.5]
+
+        n_pos_range = np.arange(500, 5500, 500)
+        print("N_pos range", n_pos_range)
+        n_neg_range = np.arange(500, 5500, 500)
+        print("N_neg range", n_neg_range)
+    elif args.param == "choice_method":
+        n_pos_range = [500]
+        n_neg_range = [500]
+        align_methods = ['s4a']
+        cls_names = ['cosine_025', 'cosine_050', 'cosine_075', 'cosine_090']
+        cls_func = [cosine_cls, cosine_cls, cosine_cls, cosine_cls]
+        cls_thresholds = [0.25, 0.5, 0.75, 0.90]
+        classifiers = list(zip(cls_names, cls_func, cls_thresholds))
+        r_range = [2.5]
+        choice_methods = ['random', 'far', 'close']
+
+    if not args.no_semeval:
+        for lang in languages:
+            wv1, wv2, targets, y_true = read_semeval_data(lang, normalized)  
+            targets_1 = targets_2 = targets
+
+            # For SemEval we repeat `target` in targets_1 and targets_2
+            results = run_experiments(wv1, wv2, targets_1, targets_2, y_true,
+                            r_range, n_pos=n_pos_range, n_neg=n_neg_range, 
+                            choice_method=choice_methods, align_method=align_methods,
+                            classifier=classifiers)
+
+
+
+            for h, r in results:
+                print(lang, r)
+                print('semeval_'+lang, normalized, *r, sep=',')
+                print('semeval_'+lang, normalized, *r, sep=',', file=fout)
+    
+    if not args.no_ukus:
+            wv1, wv2, targets, y_true = read_ukus_data(normalized)
+            targets_1, targets_2 = zip(*targets)
+
+            results = run_experiments(wv1, wv2, targets_1, targets_2, y_true,
+                            r_range, n_pos=n_pos_range, n_neg=n_neg_range, 
+                            choice_method=choice_methods, align_method=align_methods,
+                            classifier=classifiers)
+            for h, r in results:
+                print("ukus", r)
+                print("ukus", normalized, *r, sep=',')
+                print("ukus", normalized, *r, sep=',', file=fout)           
+
+    fout.close()
+
+
+if __name__ == "__main__":
+    new_main()
